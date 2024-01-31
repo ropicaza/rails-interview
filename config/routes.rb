b3a6b38dd42dd69b42f881f0e19782b7d13c5bb5
Rocio Picaza <rocio.picaza@gmail.com>
2024-01-30 22:04:47 -0300

    ⚡️ add todo_list routes for create destroy and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :todo_lists, only: %i[index], path: :todolists do
+    resources :todo_lists, only: %i[index create destroy update], path: :todolists do
       resources :todo_items, only: %i[index create destroy update], path: :todoitems
     end
   end
