ab26d933209aff1c065de277b579dad6b630b6f7
Rocio Picaza <rocio.picaza@gmail.com>
2024-01-30 21:06:51 -0300

    💄 add destroy and update methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :todo_lists, only: %i[index], path: :todolists do
-      resources :todo_items, only: %i[index create], path: :todoitems
+      resources :todo_items, only: %i[index create destroy update], path: :todoitems
     end
   end
 
