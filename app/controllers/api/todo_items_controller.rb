28fa8bbfd439d2a24e9224cabc20aa560cb87569
Rocio Picaza <rocio.picaza@gmail.com>
2024-01-30 21:09:00 -0300

    ✨ create destroy, and update methods

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -10,10 +10,35 @@ module Api
     end
 
     def create
-      TodoItem.create!(todo_item_params)
+      todo_item = TodoItem.new(todo_item_params)
+
+      if todo_item.save
+        render json: todo_item, status: :created
+      else
+        render json: todo_item.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      todo_item = find_todo_item
+      if todo_item.destroy
+        head :no_content
+      else
+        render json: todo_item.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      todo_item = find_todo_item
+
+      todo_item.update(todo_item_params)
     end
 
     private
+
+    def find_todo_item
+      TodoItem.find(params[:id])
+    end
 
     def todo_item_params
       params.require(:todo_item).permit(:name, :description, :complete, :todo_list_id)
